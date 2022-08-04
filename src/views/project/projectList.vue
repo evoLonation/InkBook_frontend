518af10c52b51bddd640f2d2f052dd715f755244
Katock <787288968@qq.com>
2022-08-04 18:55:42 +0800

    lyh

--- src/views/project/projectList.vue
+++ src/views/project/projectList.vue
@@ -26,7 +26,7 @@
             <span>{{projects[i-1].name}}</span>
             <div class="bottom">
               <text class="text">{{ projects[i-1].detail }}</text>
-              <el-button type="primary" class="button" style="width: 50px" @click="openProject">进入</el-button>
+              <el-button type="primary" class="button" style="width: 50px" @click="curProjectId=projects[i-1].id; openProject">进入</el-button>
     <!--            删除项目对话框-->
               <el-popconfirm
                   confirmButtonText="确定"

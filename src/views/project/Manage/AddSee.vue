78e5f503e45f29855c29d9dad7b5e302c7f4fbbe
XingruJiang <boxuan10258@qq.com>
2022-08-05 01:22:25 +0800

    jxr: 完成了uml图安置界面

--- src/views/project/Manage/AddSee.vue
+++ src/views/project/Manage/AddSee.vue
@@ -5,20 +5,49 @@
     <el-button
         style="width: 100px; margin: 20px 50px auto 1410px"
         type="success"
+        @click="dialogVisable = true"
     >
-      添加项目
+      添加图表
     </el-button>
+    <el-dialog
+      v-model="dialogVisable"
+      title="添加图表"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>图表名称</span>
+      <el-input
+        v-model="graphName"
+        placeholder="请输入图表名称"
+        style="margin-top: 10px"
+      >
+      </el-input>
+      <el-button
+          type="success"
+          @click="createGraph"
+          style="margin: 20px 40px auto 310px"
+      >
+        确认
+      </el-button>
+      <el-button
+          @click="handleClose"
+          style="margin: 20px 0 auto 0;"
+          type="danger"
+      >
+        取消
+      </el-button>
+    </el-dialog>
   </div>
   <el-row
       style="z-index: 2; float: left; top: -550px; bottom: -550px"
   >
     <div
-        v-for="o in 10"
-        :key="o"
+        v-for="graph in graphList"
+        :key="graph"
     >
       <el-card
           :body-style="{ padding: '0px' }"
-          style="width: 250px; height: 350px; margin: 30px 30px 30px 30px"
+          style="width: 250px; height: 350px; margin: 30px 25px 30px 25px"
       >
 <!--        <img-->
 <!--            src="../../assets/Project/UML图布局.jpeg"-->
@@ -27,15 +56,15 @@
 <!--            alt=""-->
 <!--        />-->
         <img
-            src="../../../assets/Project/设计原型.jpeg"
+            src="../../../assets/Project/UML图布局.jpeg"
             class="image"
             style="height: 260px; object-fit: cover"
             alt=""
         />
         <div style="padding: 14px">
-          <span>Yummy hamburger</span>
+          <span>{{graph.docName}}</span>
           <div class="bottom">
-            <time class="time">{{ currentDate }}</time>
+            <span>{{graph.name}}: {{graph.ModifyInfo}}</span>
             <el-dropdown style="width: 100px">
               <span class="el-dropdown-link">
                 操作
@@ -53,7 +82,7 @@
                   </el-dropdown-item>
                   <el-dropdown-item
                       style="color: #F56C6C"
-                      @click="this.$message.error('您的文件已经删除')"
+                      @click="deleteGraph(graph.graphId)"
                   >
                     删除文件
                   </el-dropdown-item>
@@ -67,10 +96,79 @@
   </el-row>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
+<script>
+import {ElMessage} from "element-plus";
 
-const currentDate = ref(new Date())
+
+export default {
+  data(){
+    return{
+      dialogVisable: false,
+      currentDate: new Date(),
+      graphName: '',
+      projectId: -1,
+      graphList: null,
+    }
+  },
+  methods: {
+    handleClose: function (){
+      this.dialogVisable = false;
+      ElMessage.warning('构建图表取消')
+    },
+    createGraph: function (){
+      this.$axios.post("graph/create", {
+        "name": this.graphName,
+        "creatorId": this.$store.state.loginUser.userId,
+        "projectId": this.projectId
+      }).then(res=>{
+        if(res.status === 200){
+          this.$message.success("构建图表成功！");
+          location.reload();
+        }
+      }).catch(err=>{
+        console.log(err);
+        this.$message.error(err.response.data.msg);
+      })
+    },
+    deleteGraph: function (graphId){
+      console.log(typeof (graphId));
+      console.log(graphId)
+      this.$axios.post("graph/complete-delete", {
+        "graphId": graphId,
+        "deleterId": this.$store.state.loginUser.userId
+      }).then(res=>{
+        if(res.status === 200){
+          this.$message.success("删除成功！");
+          location.reload();
+        }
+      }).catch(err=>{
+        console.log(err);
+        this.$message.error(err.response.data.msg);
+      })
+    },
+    getGraphList: function (){
+      console.log("getGraphList is called!")
+      this.$axios.get("graph/list", {
+        params:{
+          projectId: this.projectId
+        }
+      }).then(res=>{
+        if(res.status === 200){
+          console.log(res.data)
+          this.graphList = res.data.graphList;
+        }
+      }).catch(err=>{
+        console.log(err);
+        this.$message.error(err.response.data.msg);
+      })
+    }
+  },
+  created() {
+    this.projectId = parseInt(this.$route.params.projectId);
+    this.getGraphList();
+  }
+}
+
 </script>
 
 <style>

f9fbab121897083c99eb74a099e3422887bc5002
evoLonation <1838940019@qq.com>
2022-08-08 00:27:33 +0800

    zzy: 可以直接引入elementui的组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <antv/>
+  <my-giao/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import antv from './antv.vue'
+import myGiao from "@/MyGiao";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    antv,
+    myGiao,
   }
 }
 </script>
---
+++ src/antv.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="container"></div>
+  <my-giao/>
+</template>
+
+<script>
+import { Graph } from '@antv/x6';
+import '@antv/x6-vue-shape';
+import {onMounted} from "vue";
+import MyGiao from "./MyGiao"
+import { ElButton } from 'element-plus';
+export default {
+  components: {MyGiao},
+  setup() {
+    const initGraph = () => {
+      const graph = new Graph({
+        container: document.getElementById("container"),
+        width: 600,
+        height: 400,
+        grid: true,
+      });
+      Graph.registerNode("giao", {
+        inherit: "vue-shape",
+        x: 200,
+        y: 150,
+        width: 150,
+        height: 100,
+        component: {
+          template: `<my-giao />`,
+          components: {MyGiao},
+        },
+      });
+      Graph.registerNode("button", {
+        inherit: "vue-shape",
+        x: 100,
+        y: 150,
+        width: 150,
+        height: 100,
+        component: {
+          template: `<el-button>asdfasdghg</el-button>`,
+          components: {ElButton},
+        },
+      });
+      graph.addNode({
+        id: "1",
+        shape: "giao",
+        x: 400,
+        y: 150,
+        width: 150,
+        height: 100,
+        data: {
+          num: 0,
+        },
+      });
+      graph.addNode({
+        id: "2",
+        shape: "button",
+        x: 300,
+        y: 150,
+        width: 150,
+        height: 100,
+        data: {
+          num: 0,
+        },
+      });
+    };
+
+
+    onMounted(() => {
+      initGraph();
+    })
+
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

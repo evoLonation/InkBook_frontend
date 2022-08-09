d9c2196ace5a2f70a6d53ab30a7302b74cb3b5f1
evoLonation <1838940019@qq.com>
2022-08-09 11:46:46 +0800

    zzy: 创建团队文档的弹窗

--- src/components/dialog/CreateDocument.vue
+++ src/components/dialog/CreateDocument.vue
@@ -5,24 +5,26 @@
       width="30%"
       :before-close="newDocName = ''"
   >
-    <div class="confirm">
-      <div class="confirm-wrapper">
-        <div class="confirm-content">
-          <div style="height: 30px"></div>
-          <header style="line-height: 30px;text-align: center; font-weight: bold;margin-bottom: 20px;">创建新文档</header>
-          <div style="margin-left: 40px;margin-right: 50px;">
-            <el-form  label-width="100px">
-              <el-form-item label="文档名">
-                <el-input v-model="newDocName" placeholder="文档名称" style=" !important;margin-left: 20px;margin-right: 0;"></el-input>
-              </el-form-item>
-            </el-form>
-            <el-button type="primary" style="bottom: 30px; left: 100px; " @click="createClick"><span>确定</span></el-button>
-            <el-button type="primary" style="bottom: 30px; right: 100px; " @click="visible=false"><span>取消</span></el-button>
-          </div>
+    <div >
+      <div style="height: 30px"></div>
+      <header style="line-height: 30px;text-align: center; font-weight: bold;margin-bottom: 20px;">创建新文档</header>
+      <div style="margin-left: 40px;margin-right: 50px;">
+        <el-form  label-width="100px">
+          <el-form-item label="文档名">
+            <el-input v-model="newDocName" placeholder="文档名称" style=" !important;margin-left: 20px;margin-right: 0;"></el-input>
+          </el-form-item>
+        </el-form>
+        <div style="display: flex;margin-top: 40px;">
+          <el-button type="primary" style="margin: auto auto auto 40px " @click="createClick"><span>确定</span></el-button>
+          <el-button type="primary" style="margin: auto 40px auto auto " @click="value=false"><span>取消</span></el-button>
         </div>
+
       </div>
+      <div style="height: 30px"></div>
     </div>
+
   </el-dialog>
+
 </template>
 
 <script>
@@ -32,7 +34,6 @@ export default {
   name: "CreateDocument",
   data() {
     return {
-      visible: false,
       newDocName: '',
       userId: this.$store.state.loginUser.userId,
 
@@ -72,7 +73,22 @@ export default {
   }
 }
 </script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.el-dialog {
+  border-radius: 40px;
+  object-fit:cover;
+  width: 450px;
 
-<style scoped>
-
+}
+.el-dialog__body{
+}
+.el-dialog__header {
+  height: 0;
+  padding: 0;
+}
 </style>
+

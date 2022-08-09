9f38f1f89ff92bc711de363a1342e92099b210df
evoLonation <1838940019@qq.com>
2022-08-09 17:26:19 +0800

    zzy: 完成文档回收站

--- src/views/docCenter/fileRec.vue
+++ src/views/docCenter/fileRec.vue
@@ -2,55 +2,19 @@
   <div
     class="folder-layout"
   >
-    <div
-      style="display: flex"
-    >
-      <el-input
-          v-model="search"
-          class="folder-el-input"
-          placeholder="输入文件名称"
-      >
-        <template #prefix>
-          <el-icon>
-            <search/>
-          </el-icon>
-        </template>
-        <template #append>
-          <el-button
-            style="font-weight: bold"
-          >
-            搜索
-          </el-button>
-        </template>
-      </el-input>
-      <el-button
-        class="folder-add-button"
-        @click="visible=true"
-      >
-        <el-icon><plus/></el-icon>&nbsp;新建文件&nbsp;
-      </el-button>
-    </div>
+    <div style="height: 40px"></div>
     <div
       class="folder-guide"
     >
       <el-button
           class="folder-roll-back"
-          :disabled="this.folderLayer.length === 1"
-          @click="clickBack"
       >
-        <el-icon>
-          <arrow-left/>
-        </el-icon>
+        <el-icon size="large"><DeleteFilled /></el-icon>
       </el-button>
-      <span v-if="!isMove"
+      <span
           class="folder-guide-words"
       >
-        {{this.folderLayer[this.folderLayer.length - 1].folderName}}
-      </span>
-      <span v-else
-          class="folder-guide-words"
-      >
-        移动到...
+        文档回收站
       </span>
     </div>
     <el-scrollbar height="75vh">
@@ -58,11 +22,11 @@
         <v-contextmenu ref="contextmenu">
           <v-contextmenu-item
               class="my-context-item"
-              style="color: #409EFF;"
-              @click="this.$refs.rename.open(this.currentId, this.currentType)"
+              style="color: green;"
+              @click="clickRecover"
           >
             <el-icon><edit/></el-icon>
-            &nbsp;重命名
+            &nbsp;恢复
           </v-contextmenu-item>
           <v-contextmenu-item
               class="my-context-item"
@@ -70,38 +34,17 @@
               @click="clickDelete"
           >
             <el-icon><delete/></el-icon>
-            &nbsp;删除
-          </v-contextmenu-item>
-          <v-contextmenu-item
-              class="my-context-item"
-              style="color: brown"
-              @click="clickMove"
-          >
-            <el-icon><TopRight /></el-icon>
-            &nbsp;{{this.folderLayer.length === 1 ? "移动" : "移到根目录"}}
-          </v-contextmenu-item>
-          <v-contextmenu-item
-              class="my-context-item"
-              style="color: black"
-          >
-            <el-icon><setting/></el-icon>
-            &nbsp;属性
+            &nbsp;彻底删除
           </v-contextmenu-item>
 
         </v-contextmenu>
-        <div class="folder-info" v-for="doc of this.docList" :key="doc" v-contextmenu:contextmenu @click="this.clickDoc(doc.docId)" @contextmenu.prevent="saveCurrent(doc.docId, 'doc')">
+        <div class="folder-info" v-for="doc of this.docList" :key="doc" v-contextmenu:contextmenu @contextmenu.prevent="currentId = doc.docId">
           <img class="folder-picture" src="../../assets/docCenter/文件试用.png"  alt="">
           <span class="folder-name" >{{doc.docName}}</span>
-        </div>
-        <div class="folder-info" v-for="folder of this.folderList" :key="folder" v-contextmenu:contextmenu @click="this.clickFolder(folder.folderId, folder.name)" @contextmenu.prevent="saveCurrent(folder.folderId, 'folder')">
-          <img class="folder-picture" src="../../assets/docCenter/文件夹试用.png" alt="">
-          <span class="folder-name" >{{folder.name}}</span>
         </div>
       </el-row>
     </el-scrollbar>
   </div>
-  <CreateFile v-model="visible" @new-created="this.getListFiles" :parent-id="this.folderLayer[this.folderLayer.length - 1].folderId" type="team"/>
-  <Rename v-model="renameVisible" ref="rename" @have-renamed="getListFiles"/>
 </template>
 
 <script>
@@ -115,14 +58,9 @@ import Rename from "@/components/dialog/Rename";
 export default {
   name: "FolderList",
   components: {
-    Setting,
-    ArrowLeft,
     Delete,
-    Plus, Search,
     [Contextmenu.name]: Contextmenu,
     [ContextmenuItem.name]: ContextmenuItem,
-    CreateFile,
-    Rename
 
   },
   directives: {
@@ -130,26 +68,12 @@ export default {
   },
   data(){
     return{
-      search: '',
-      visible: false,
-      renameVisible: false,
-      folderLayer: [
-        {
-          folderId: 0,
-          folderName: '根目录',
-        },
-      ],
-      folderList: [],
       docList: [],
       teamId: this.$store.state.selectTeam.teamId,
       userId: this.$store.state.loginUser.userId,
 
-      isMove : false,
-
-      moverId : 0,
 
       currentId: 0,
-      currentType: 'doc',
     }
   },
   methods: {
@@ -157,96 +81,39 @@ export default {
       this.$message.success("success!");
     },
     getListFiles() {
-      this.axios.get('file/list', {
+      this.axios.get('document/recycle', {
         params: {
           "teamId": this.teamId,
-          "parentId" : this.folderLayer[this.folderLayer.length - 1].folderId,
         }
       }).then((res) => {
-        this.folderList = res.data.folderList;
         this.docList = res.data.docList;
       }).catch(err => {
         console.log(err)
         ElMessage({message: err.response.data.msg, type: 'warning'})
       })
     },
-    clickDoc(docId) {
-      this.$router.push({name: 'DocumentEdit', params:{docId: docId}})
-    },
-    clickFolder(folderId, folderName) {
-      if(!this.isMove){
-        this.folderLayer.push( {
-          folderId: folderId,
-          folderName: folderName,
-        })
-        this.getListFiles();
-      }else{
-        this.axios.post('folder/move', {
-          docId : this.moverId,
-          folderId: folderId,
-        }).then(() => {
-          ElMessage({message: '移动成功', type: 'success'});
-          this.getListFiles();
-          this.isMove = false;
-        }).catch(err =>{
-          console.log(err)
-          ElMessage({message: err.response.data.msg, type: 'warning'})
-        })
-      }
-
-
+    clickRecover() {
+      this.axios.post('document/recover', {
+        docId: this.currentId,
+      }).then((res) => {
+        ElMessage({message: '恢复成功', type: 'success'});
+        this.getListFiles()
+      }).catch(err => {
+        console.log(err)
+        ElMessage({message: err.response.data.msg, type: 'warning'})
+      })
     },
     clickDelete() {
-      if(this.currentType === 'doc'){
-        this.axios.post('document/delete', {
+        this.axios.post('document/complete-delete', {
           docId : this.currentId,
-          deleterId : this.userId,
         }).then(() => {
-          ElMessage({message: '删除成功', type: 'success'});
+          ElMessage({message: '彻底删除成功', type: 'success'});
           this.getListFiles()
         }).catch(err =>{
           console.log(err)
           ElMessage({message: err.response.data.msg, type: 'warning'})
         })
-      }else {
-        this.axios.post('folder/complete-delete', {
-          folderId : this.currentId,
-          deleterId : this.userId,
-        }).then(() => {
-          ElMessage({message: '删除成功', type: 'success'});
-          this.getListFiles()
-        }).catch(err =>{
-          console.log(err)
-          ElMessage({message: err.response.data.msg, type: 'warning'})
-        })
-      }
-    },
-    clickBack() {
-      this.folderLayer.pop()
-      this.getListFiles()
-    },
-    saveCurrent(id, type) {
-      this.currentId = id;
-      this.currentType = type;
-    },
-    clickMove() {
-      this.moverId = this.currentId;
-      if(this.folderLayer.length === 1){
-        this.isMove = true;
-      }else{
-        this.axios.post('folder/move', {
-          docId : this.moverId,
-          folderId: 0,
-        }).then(() => {
-          ElMessage({message: '移动成功', type: 'success'});
-          this.getListFiles();
-          this.isMove = false;
-        }).catch(err =>{
-          console.log(err)
-          ElMessage({message: err.response.data.msg, type: 'warning'})
-        })
-      }
-    },
+    }
   },
   created() {
     this.getListFiles();
